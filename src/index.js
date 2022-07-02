d6d5245f6abccfb0dfefede73dfa2e96b07a67b9
Atlas_WebDev <web.dev89@yandex.by>
2022-07-02 20:16:37 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    
+        //Function defactory
+        const format = (data) => {
+            // Get array in input data
+            const arr = String(data).split('');
+                if (arr[arr.length - 1] === '0') {
+                    arr.pop();
+                }
+                if(arr[0] === '-') {
+                    arr.shift();
+                }
+            return arr.reverse().join('');
+        }
+
+        const result = format(n);
+    return result;
 }
